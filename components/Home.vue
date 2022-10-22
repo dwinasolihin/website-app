a3b4e0425eec4586b539dfb50eb8ba947e7828b0
Dwina Solihin <dwinasolihin@gmail.com>
2022-10-22 00:15:09 -0700

    trying new setup

--- components/Home.vue
+++ components/Home.vue
@@ -1,10 +1,12 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div id="IndexPage">
-    <nav-bar></nav-bar>
-    <p class="headline">{{homesObj}}</p>
-    <p class="headline">{{blogsObj}}</p>
-  </div>
+<div id="app">
+  <nav-bar></nav-bar>
+
+  <button @click="count++">
+  Count is: {{ count }}
+  </button>
+</div>
 </template>
 
 <script>
@@ -27,31 +29,31 @@ export default {
   },
   methods: {
     getHomes() {
-      return axios.get(config.url + config.bucket_slug + '/object-type/homes', {
-        params: { 
-          read_key: config.read_key 
-        } 
-      });
-    },
-    getBlogs() {
-      return axios.get(config.url + config.bucket_slug + '/object-type/blogs', {
-        params: { 
-          read_key: config.read_key 
-        } 
-      });
-    },
-    async getSupportingMeta() {
-      try {
-        const homesResponse = await this.getHomes();
-        this.homesObj = homesResponse.data.objects[0];
-        console.log(this.homesObj);
-        const blogsResponse = await this.getBlogs();
-        this.blogsObj = blogsResponse.data.objects[0];
-        console.log(this.blogsObj);
-      } catch (error) {
-        console.log(error); // eslint-disable-line
-      }
-    },
+        return axios.get(config.url + config.bucket_slug + '/object-type/homes', {
+          params: { 
+            read_key: config.read_key 
+          } 
+        });
+      },
+      getBlogs() {
+        return axios.get(config.url + config.bucket_slug + '/object-type/blogs', {
+          params: { 
+            read_key: config.read_key 
+          } 
+        });
+      },
+      async getSupportingMeta() {
+        try {
+          const homesResponse = await this.getHomes();
+          this.homesObj = homesResponse.data.objects[0];
+          console.log(this.homesObj);
+          const blogsResponse = await this.getBlogs();
+          this.blogsObj = blogsResponse.data.objects[0];
+          console.log(this.blogsObj);
+        } catch (error) {
+          console.log(error); // eslint-disable-line
+        }
+      },
   },
 }; 
 </script>
